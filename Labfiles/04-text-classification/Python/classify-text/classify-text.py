e13b4449dd11afbb78219c90156d6c7aa14bca86
Peter Rooke <peter.rooke@kainos.com>
2025-06-26 16:15:20 +0100

    Solutions

--- Labfiles/04-text-classification/Python/classify-text/classify-text.py
+++ Labfiles/04-text-classification/Python/classify-text/classify-text.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -14,7 +15,9 @@ def main():
         deployment_name = os.getenv('DEPLOYMENT')
 
         # Create client using endpoint and key
-
+        AzureKeyCredential credentials = new AzureKeyCredential(aiSvcKey);
+        Uri endpoint = new Uri(aiSvcEndpoint);
+        TextAnalyticsClient aiClient = new TextAnalyticsClient(endpoint, credentials);
 
         # Read each text file in the articles folder
         batchedDocuments = []
@@ -26,12 +29,27 @@ def main():
             batchedDocuments.append(text)
 
         # Get Classifications
+        operation = ai_client.begin_single_label_classify(
+            batchedDocuments,
+            project_name=project_name,
+            deployment_name=deployment_name
+        )
 
+        document_results = operation.result()
 
+        for doc, classification_result in zip(files, document_results):
+            if classification_result.kind == "CustomDocumentClassification":
+                classification = classification_result.classifications[0]
+                print("{} was classified as '{}' with confidence score {}.".format(
+                    doc, classification.category, classification.confidence_score)
+                )
+            elif classification_result.is_error is True:
+                print("{} has an error with code '{}' and message '{}'".format(
+                    doc, classification_result.error.code, classification_result.error.message)
+                )
 
     except Exception as ex:
         print(ex)
-
 
 if __name__ == "__main__":
     main()
